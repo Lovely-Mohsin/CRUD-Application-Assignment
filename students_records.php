72989943790e3ad427a60d00ab787b0e48b0ae6a
Lovely-Mohsin <153726650+Lovely-Mohsin@users.noreply.github.com>
2024-03-24 20:55:00 -0700

    Students-Records File created
    Bootstrap file added and Students Records File Created

---
+++ students_records.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Students-Records</title>
+    <link rel="stylesheet" href="./Bootstrap/css//bootstrap.min.css" class="css">
+    <script src="./Bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
